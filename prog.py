1ea2f64d18a02376f60e45e91df29ebf8b379e1a
Ульяна-titanyuk <uliana-titanyuk@mail.ru>
2020-06-01 19:00:15 +0300

    change 1

--- prog.py
+++ prog.py
@@ -4,18 +4,18 @@ name_of_file = data.txt
 
 def Sequence(filename):
     t = 0
-    n=0
-    s = 0
+    n = 0
+    k = 0
     try:
         f=open(filename, "r")
          for s in [s5 for s1 in f for s2 in s1.split(' ') for s3 in s2.split('\n') for s4 in s3.split(',') for s5 in s4.split('\t') if s5!='']: 
            try:
-             s+=1
+             k+=1
         for s in [s5 for s1 in f for s2 in s1.split(' ') for s3 in s2.split('\n') for s4 in s3.split(',') for s5 in s4.split('\t') if s5!='']: 
             try:
                 t=int(s)-1
                 n+=1
-                if n<s
+                if n<k
                  if (n>0)
                  print(t,"*x^",n,"+") 
             except:
