bbca9aba3979fe10a7690c503743610565c93838
Ульяна-titanyuk <uliana-titanyuk@mail.ru>
2020-06-01 22:15:48 +0300

    +

--- prog.py
+++ prog.py
@@ -1,35 +1,48 @@
 import os
-name_of_file = data.txt
+name_of_file = "data.txt"
 
 
 def Sequence(filename):
     t = 0
     n = 0
     k = 0
+    res1 = "" #res1 = 0
+    res2 = ""  # res2 = 0
     try:
         f=open(filename, "r")
-         for s in [s5 for s1 in f for s2 in s1.split(' ') for s3 in s2.split('\n') for s4 in s3.split(',') for s5 in s4.split('\t') if s5!='']: 
-           try:
-             k+=1
-        for s in [s5 for s1 in f for s2 in s1.split(' ') for s3 in s2.split('\n') for s4 in s3.split(',') for s5 in s4.split('\t') if s5!='']: 
-            try:
-                n+=1
-                t=int(s)*(n-1)
-                if n<k
-                 if (n>0)
-                 print(t,"*x^",n-1,"+") 
-            except:
-                print('word ',s,' ignored',sep='')
-       
-            return res
-        elif (n==k):
-            print(t,"*x^",k-1)
-        elif (n==0):
-            return 0
+        words = [s5 for s1 in f for s2 in s1.split(' ') for s3 in s2.split('\n') for s4 in s3.split(',') for s5 in s4.split('\t') if s5!='']
+        k = len(words)
+        # for _ in words:
+        #     k+=1
+        if k == 0:
+            print(0)
+        else:
+            for s in words[::-1]:
+                try:
+                    t=int(s)*(n)
+                    if (n == 0):
+                        if k==1:
+                            res1 += "0" #
+                            res2 += str(s) + "*x^" + str(n)  # res2 += s*(x**(n))
+                        else:
+                            res2 += str(s) + "*x^" + str(n) + " + "  # res2 += s*(x**(n))
+                    elif n<(k-1):
+                        if (n>0):
+                            res1 += str(t)+"*x^"+str(n-1)+" + "  #res1 += t*(x**(n-1))
+                            res2 += str(s) + "*x^" + str(n) + " + "  # res2 += s*(x**(n))
+                    elif (n == (k-1)):
+                        res1 += str(t) + "*x^" + str(n - 1) #res1 += t*(x**(n-1))
+                        res2 += str(s) + "*x^" + str(n)  # res2 += s*(x**(n))
+                except:
+                    print('word ',s,' ignored',sep='')
+                n += 1
         f.close()
+        return res1, res2
     except FileNotError:
         print("Can't open file")
         return None
 
 
-a = Sequence(name_of_file)
+a, b = Sequence(name_of_file)
+print("function derivative = ", a)
+print("function = ", b)
